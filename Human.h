92acd87d949cfa1619a6b65a73514bb59a6f5d8c
X2King <amirx2k@gmail.com>
2023-08-07 21:37:56 +0930

    week 2 finished

--- Human.h
+++ Human.h
@@ -1,9 +1,14 @@
 #ifndef HUMAN_H
 #define HUMAN_H
 #include "Player.h"
+#include "Move.h"
+#include <string>
 
 class Human: public Player
 {
+    private:
+    string name;
+    public:
     Human();
     Human(string name_in);
     Move* makeMove();
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "Human.h"
+#include "Computer.h"
+#include "Referee.h"
+#include <iostream>
+
+int main() {
+    // Create the players and referee
+    Human humanPlayer();
+    Computer computerPlayer;
+    Referee referee;
+
+    // Play the game and get the result
+    Player* winner = referee.judgeWinner(&humanPlayer, &computerPlayer);
+
+    // Print the result
+    if (winner == nullptr) {
+        std::cout << "It's a Tie." << std::endl;
+    } else {
+        std::cout << winner->getName() << " Wins." << std::endl;
+    }
+
+    return 0;
+}
