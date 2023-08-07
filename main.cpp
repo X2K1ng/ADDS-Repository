66763a0a117d82bae84e699f8396ef0a9975731a
X2King <amirx2k@gmail.com>
2023-08-07 22:51:55 +0930

    week 2 finished

--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
+#include <iostream>
+#include "Player.h"
 #include "Human.h"
 #include "Computer.h"
 #include "Referee.h"
-#include <iostream>
 
 int main() {
-    // Create the players and referee
-    Human humanPlayer;
+    std::cout << "Welcome to the Monkey-Robot-Pirate-Ninja-Zombie game!" << std::endl;
+    
+    Human humanPlayer("Ash");
     Computer computerPlayer;
+    
     Referee referee;
-
-    // Play the game and get the result
-    Player* winner = referee.refGame(&humanPlayer, &computerPlayer);
-
-    // Print the result
-    if (winner == nullptr) {
-        std::cout << "It's a Tie." << std::endl;
-    } else {
-        std::cout << winner->getName() << " Wins." << std::endl;
+    
+    for (int i = 0; i < 3; ++i) {
+        std::cout << "Round " << i+1 << std::endl;
+        
+        Player* winner = referee.refGame(&humanPlayer, &computerPlayer);
+        
+        if (winner == nullptr) {
+            std::cout << "It's a Tie." << std::endl;
+        } else {
+            std::cout << winner->getName() << " Wins." << std::endl;
+        }
     }
 
     return 0;
