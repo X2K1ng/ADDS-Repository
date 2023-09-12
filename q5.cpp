fdfe3aedb1a29490d1962c0f34ba3edf9e681036
X2K1ng <83744567+X2K1ng@users.noreply.github.com>
2023-09-12 17:35:33 +0930

    initial commit

--- q5.cpp
+++ q5.cpp
@@ -6,18 +6,17 @@ private:
     /* data */
 public:
     Rectangle(/* args */);
+    Rectangle(int,int);
     ~Rectangle();
     int length;
     int width;
 
 };
 
-Rectangle::Rectangle() : length(0), width(0)
-{
+Rectangle::Rectangle() : length(0), width(0){
 }
 
-Rectangle::Rectangle(int l,int w) : length(l), width(w)
-{
+Rectangle::Rectangle(int l,int w) : length(l), width(w){
 }
 
 Rectangle::~Rectangle()
