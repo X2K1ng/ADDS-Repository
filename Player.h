d54b9deebf290463deda61dd1110a0219630c486
X2K1ng <83744567+X2K1ng@users.noreply.github.com>
2023-09-29 17:07:04 +0930

    change

---
+++ Player.h
@@ -0,0 +1,24 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Player
+{
+private:
+
+public:
+    string name;
+    Player();
+    ~Player();
+};
+
+Player::Player()
+{
+}
+
+Player::~Player()
+{
+}
+#endif
